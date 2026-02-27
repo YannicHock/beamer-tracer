df17a3198babc20db08822128bd6a263e93ec119
Yannic Hock <mail@yannic-hock.de>
2026-02-27 15:39:02 +0100

    feat: add Electron main process

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    fullscreen: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  win.loadFile('index.html');
+  win.setMenuBarVisibility(false);
+}
+
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => app.quit());
